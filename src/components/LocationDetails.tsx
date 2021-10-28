e7399609093e0190258e96dd78a10a9b96895730
kamini14895 <85287571+kamini14895@users.noreply.github.com>
2021-10-28 16:31:56 +0530

    Update the Location Link name

--- src/components/LocationDetails.tsx
+++ src/components/LocationDetails.tsx
@@ -9,7 +9,6 @@ const LocationDetails: React.FC = () => {
   const [fetching] = useState(false);
   
   const [weatherDetails] = useState<WeatherInfo[]>([{"dayName":"Wed","dayOrdianl":"27th","maxTemp":"17°","minTemp":"13°","icon":"https://www.metaweather.com/static/img/weather/hc.svg","stateName":"Heavy Cloud","windSpeed":"21kmph","humidity":"74%"},{"dayName":"Thu","dayOrdianl":"28th","maxTemp":"17°","minTemp":"12°","icon":"https://www.metaweather.com/static/img/weather/lc.svg","stateName":"Light Cloud","windSpeed":"20kmph","humidity":"71%"},{"dayName":"Fri","dayOrdianl":"29th","maxTemp":"15°","minTemp":"11°","icon":"https://www.metaweather.com/static/img/weather/lr.svg","stateName":"Light Rain","windSpeed":"23kmph","humidity":"80%"},{"dayName":"Sat","dayOrdianl":"30th","maxTemp":"14°","minTemp":"11°","icon":"https://www.metaweather.com/static/img/weather/lr.svg","stateName":"Light Rain","windSpeed":"21kmph","humidity":"84%"},{"dayName":"Sun","dayOrdianl":"31st","maxTemp":"15°","minTemp":"10°","icon":"https://www.metaweather.com/static/img/weather/hr.svg","stateName":"Heavy Rain","windSpeed":"20kmph","humidity":"81%"}]);
-  const [cityName] = useState("")
 
   let template = <LoadingSpinner></LoadingSpinner>;
 
@@ -22,7 +21,6 @@ const LocationDetails: React.FC = () => {
             <Breadcrumb.Item  linkAs={Link} linkProps={{to: "/"}}>
                 View Locations
             </Breadcrumb.Item>
-            <Breadcrumb.Item active>{cityName}</Breadcrumb.Item>
             </Breadcrumb>
                
             </Col>
